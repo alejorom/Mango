3f5d15c1465023648136dec1b46b23de79c793f4
AlejandroRR <rromeror@indracompany.com>
2026-05-12 10:05:41 -0500

    Adicionar servicio JWT

---
+++ Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,61 @@
+﻿using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtTokenGenerator(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        /// <summary>
+        /// Genera un token JWT firmado para un usuario específico.
+        /// </summary>
+        /// <param name="applicationUser">Objeto que contiene la información del usuario desde la base de datos.</param>
+        /// <returns>Un string que representa el JWT codificado.</returns>
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+            // Manejador para la creación del token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Recuperamos la clave secreta desde la configuración (inyectada vía IOptions)
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+            // Definición de la carga útil (Payload)
+            var claimList = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email, applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name, applicationUser.UserName)
+            };
+
+            // Configuración completa del token (Encabezado y Cuerpo)
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = _jwtOptions.Issuer,
+                Audience = _jwtOptions.Audience,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.UtcNow.AddDays(7), // El token expira en una semana
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature // Algoritmo de cifrado estándar
+                )
+            };
+
+            // Creación física del objeto Token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Serialización a string (formato Base64)
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
