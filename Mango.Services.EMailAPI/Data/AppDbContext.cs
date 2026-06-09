6c8a7611b9e69d676bd58f98ecf2c37cdf5b37ba
AlejandroRR <rromeror@indracompany.com>
2026-06-09 14:45:56 -0500

    Configurar EmailAPI Inicial

---
+++ Mango.Services.EMailAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.EMailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.EMailAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+    }
+}
